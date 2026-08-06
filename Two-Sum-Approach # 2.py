01763ad80fbc5913bdfe52f58567289452f648fd
Ahsan-ops102 <ahsanumair102@gmail.com>
2026-08-06 11:52:28 +0500

    Initial commit

---
+++ Two-Sum-Approach # 2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        hashmap = {}
+        
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [hashmap[complement],i]
+            hashmap[nums[i]] = i
+        return []
