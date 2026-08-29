d0fb4aab7d229ece72c906ecda15578fc3be6bbb
Ahsan-ops102 <ahsanumair102@gmail.com>
2026-08-29 22:16:41 +0500

    Initial commit

---
+++ 349-Intersection-of-Two-Arrays-Method # 1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    if nums1[i] not in result:
+                       
+                        result.append(nums1[i])
+        return result
